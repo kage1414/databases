923fe6d7f6e73b935e14e333f05bc240e2349d27
Marcelo Yates <marceloayates@gmail.com>
2021-03-04 23:00:26 -0600

    March 4 EOD commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,31 @@
 var models = require('../models');
+var parser = require('body-parser');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // access database, models/index.js?
+      let messages = models.messages.get();
+
+      // send messages back to client
+      res.send(messages);
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {},
-    post: function (req, res) {}
+    post: function (req, res) {
+
+      console.log(parser.json());
+
+      // req.on('end', () => {
+      //   console.log
+      // })
+    }
   }
 };
 
