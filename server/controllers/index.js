4a466f15378fd6e56d2a5f4f05be09a970883e33
kage1414 <kylejohnson92294@gmail.com>
2021-03-06 17:03:36 -0500

    Refactoring to Sequelize

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -39,14 +39,12 @@ module.exports = {
   },
 
   users: {
-    get: function (req, res) {
-      models.users.get((error, results) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(results);
-        }
-      });
+    get: (req, res) => {
+      User.sync()
+        .then(() => {
+          return User.findAll({ where: req.body });
+        })
+        .catch();
     },
     post: function (req, res) {
       models.users.post(req.body, (error, results) => {
@@ -60,3 +58,23 @@ module.exports = {
   }
 };
 
+// User.sync()
+//   .then(function() {
+//     // Now instantiate an object and save it:
+//     return User.create({username: 'Jean Valjean'});
+//   })
+//   .then(function() {
+//     // Retrieve objects from the database:
+//     return User.findAll({ where: {username: 'Jean Valjean'} });
+//   })
+//   .then(function(users) {
+//     users.forEach(function(user) {
+//       console.log(user.username + ' exists');
+//     });
+//     db.close();
+//   })
+//   .catch(function(err) {
+//     // Handle any error in the chain
+//     console.error(err);
+//     db.close();
+//   });
